ab8d7cf6187aba763138d7e60c934339b39ede62
Wasim <wasimwik919@gmail.com>
2021-01-26 17:42:32 +0530

    header changes

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,17 +2,20 @@ import React from "react"
 
 import { AppBar, Toolbar, Typography, makeStyles, Switch } from "@material-ui/core"
 
-const useStyles = makeStyles({
+const useStyles = makeStyles((theme) => ({
 	appBarHeader: {
 		fontFamily: "Monoton",
 		margin: "0 auto"
+	},
+	appBar: {
+		backgroundColor: theme.palette.type === "light" ? "#90caf9" : "dark"
 	}
-})
+}))
 
 export default function Header({ dark, changeTheme }) {
 	const classes = useStyles()
 	return (
-		<AppBar color="default" position="sticky">
+		<AppBar className={classes.appBar} color="default" position="sticky">
 			<Toolbar>
 				<Typography className={classes.appBarHeader} variant="h5">
 					HI
